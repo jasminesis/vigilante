9aed7658156af8719533e5a00347d4c0a0ffccc6
Jasmine <jasmine-aips@hotmail.com>
2019-12-01 21:26:21 +0800

    renamed images

--- bounce/rock.js
+++ bounce/rock.js
@@ -1,7 +1,7 @@
 console.log('rocks falling from the sky!');
 
 let canvas = document.querySelector('canvas');
-canvas.width = window.innerWidth / 2;
+canvas.width = window.innerWidth;
 canvas.height = window.innerHeight / 2;
 let ctx = canvas.getContext('2d');
 
@@ -12,8 +12,12 @@ rock.onload = function() {
 	animate();
 };
 
+// higher gravity (0.9) makes it move and bounce faster
 var gravity = 0.1;
-var friction = 0.98;
+
+// friction has to be a fraction so that is lowers the speed in general
+// higher friction means more bounce because less speed is being "rubbed off"
+var friction = 0.1;
 
 function randomIntFromRange(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) + min);
@@ -29,16 +33,18 @@ function Rock(x, y, dx, dy) {
 	this.height = 27;
 
 	this.update = function() {
-		if (this.y + this.height / 2 + this.dy > canvas.height) {
-			// this.dy = -this.dy; //makes rocks bounce back up
-			this.dy = this.dy * friction;
-			this.dx = this.dx * friction;
-		} else {
-			this.dy += gravity;
-		}
-		if (this.x > canvas.width || this.x < canvas.width) {
-			this.dx = -this.dx * friction;
-		}
+		// if it touches the bottom of the canvas - then bounce back up and get affected by friction
+		// if (this.y + this.height / 1.2 + this.dy > canvas.height) {
+		// this.dy = -this.dy; //makes rocks bounce back up
+		// this.dy = this.dy * friction;
+		// } else {
+		// else, while it is falling, add gravity to make it fall faster
+		this.dy += gravity;
+		// }
+		// if the x position of the rock is outside the canvas OR is inside the canvas
+		// if (this.x > canvas.width || this.x < canvas.width) {
+		// 	this.dx = -this.dx * friction;
+		// }
 
 		this.x += this.dx;
 		this.y += this.dy;
@@ -58,10 +64,11 @@ var rockArray = [];
 function init() {
 	rockArray = [];
 	console.log('rockArray', rockArray);
-	for (let i = 0; i < 15; i++) {
+	for (let i = 0; i < 12; i++) {
 		var x = randomIntFromRange(0, canvas.width);
-		var y = randomIntFromRange(0, canvas.height);
-		var dx = randomIntFromRange(-3, 3);
+		// rocks fall from the top half of the screen
+		var y = randomIntFromRange(0, canvas.height / 2);
+		var dx = -5; //randomIntFromRange(-3, 3);
 		var dy = randomIntFromRange(-2, 2);
 		rockArray.push(new Rock(x, y, dx, dy));
 	}
